0565f84ba7b91a0075c01bfd1cd2f3d880bbf066
marlusmarcos <marlusufrn@gmail.com>
2023-03-27 14:43:25 -0300

    ajustando css

--- pessoal/tarefa01/src/componentes/Topo.tsx
+++ pessoal/tarefa01/src/componentes/Topo.tsx
@@ -27,19 +27,26 @@ function Topo () {
 
 
     return (
-        <>
-        <div className={styles.items}>
-        <p className={styles.topo}>
-            Carrinho de compras
-        </p>
-        <Total total={count}/>
-        <Item nome={"pizza" } descricao={"pizza com cabalbresa e Mussarela"} preco={51.5} incrementCount={incrementCount} decrementCount = { decrementCount}/>
-        <Item nome={"pizza" } descricao={"pizza frango com catupry"} preco={51.5} incrementCount={incrementCount} decrementCount = { decrementCount}/>
-        <Item nome={"pizza" } descricao={"pizza 4 queijos"} preco={51.5} incrementCount={incrementCount} decrementCount = { decrementCount}/>
-        <Item nome={"pizza" } descricao={"pizza portuguesa"} preco={51.5} incrementCount={incrementCount} decrementCount = { decrementCount}/>      
+        
+        <div className={styles.container}>
+            <div  className={styles.items}>
+                <p className={styles.topo}>
+                    Carrinho de compras
+                </p>
+                <Total total={count}/>
+                <Item nome={"pizza" } descricao={"pizza com cabalbresa e Mussarela"} preco={51.5} incrementCount={incrementCount} decrementCount = { decrementCount}/>
+                <Item nome={"pizza" } descricao={"pizza frango com catupry"} preco={51.5} incrementCount={incrementCount} decrementCount = { decrementCount}/>
+                <Item nome={"pizza" } descricao={"pizza 4 queijos"} preco={51.5} incrementCount={incrementCount} decrementCount = { decrementCount}/>
+                <Item nome={"pizza" } descricao={"pizza portuguesa"} preco={51.5} incrementCount={incrementCount} decrementCount = { decrementCount}/>      
+            </div>
+            <div className={styles.resumo}>
+                <p>Resumo</p>
+            </div>
+
         </div>
         
-        </>
+        
+        
     )
 
 }
